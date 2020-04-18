e6873afa968b089856d27ff2a00f459f7e03acf1
Daniel Mok <daniel.cs.mok@gmail.com>
2020-04-18 00:32:07 -0500

    Improving instructions scene

--- js/instructionsScene.js
+++ js/instructionsScene.js
@@ -7,20 +7,14 @@ export class InstructionsScene extends Phaser.Scene {
     }
     
     preload() {
+        this.load.image('instruction_background', 'assets/instruction_background.png');
         this.load.image('back_button', 'assets/back_button.png');
     }
 
     create() {
-        this.add.image(400, 300, 'title_background');
+        this.add.image(400, 300, 'instruction_background');
 
-        var instructionsText = this.add.text(100, 200,
-            "Stop these spring breaker degenerates from entering your city.\n" +
-            "Enforce social distancing by clicking on them with your\n" +
-            "teleporter gun to send them home.\n" +
-            "Use the number keys to select other tools at your disposal.",
-            { fill: 'yellow' });
-        
-        var backButton = this.add.image(400, 500, 'back_button');
+        var backButton = this.add.image(400, 550, 'back_button');
         backButton.setInteractive({
             useHandCursor: true
         }).on('pointerdown', this.clickBackButton, this);
--- js/titleScene.js
+++ js/titleScene.js
@@ -8,14 +8,12 @@ export class TitleScene extends Phaser.Scene {
     
     preload() {
         this.load.image('title_background', 'assets/title_background.png');
-        this.load.image('title_text', 'assets/title_text.png');
         this.load.image('title_play_button', 'assets/title_play_button.png');
         this.load.image('title_instructions_button', 'assets/title_instructions_button.png');
     }
 
     create() {
         this.add.image(400, 300, 'title_background');
-        this.add.image(400, 200, 'title_text');
         
         var playButton = this.add.image(400, 400, 'title_play_button');
         playButton.setInteractive({
